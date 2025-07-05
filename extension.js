e1c4b00923f9b8ad3cf96fff3b16417275e7ac9c
Jian Z'car Esteban <esteban.jianzcar@outlook.com>
2025-07-05 14:53:30 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,19 +1,46 @@
+import Gio from 'gi://Gio';
 import St from 'gi://St';
-
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 export default class Extension {
-    _updateColorScheme(scheme) {
-        Main.sessionMode.colorScheme = scheme;
-        St.Settings.get().notify('color-scheme');
-    }
-
     enable() {
+        this._settings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' });
+
+        // Watch for color-scheme changes
+        this._settingsChangedId = this._settings.connect('changed::color-scheme', () => {
+            this._onColorSchemeChanged();
+        });
+
+        // Apply if it's currently 'default'
+        this._onColorSchemeChanged();
+
+        // Save original sessionMode color scheme so we can restore it
         this._savedColorScheme = Main.sessionMode.colorScheme;
-        this._updateColorScheme('prefer-light');
     }
 
     disable() {
-        this._updateColorScheme(this._savedColorScheme);
+        if (this._settingsChangedId) {
+            this._settings.disconnect(this._settingsChangedId);
+            this._settingsChangedId = null;
+        }
+
+        if (this._savedColorScheme) {
+            this._updateSessionColorScheme(this._savedColorScheme);
+        }
+    }
+
+    _onColorSchemeChanged() {
+        const current = this._settings.get_string('color-scheme');
+
+        if (current === 'default') {
+            // Force it to prefer-light
+            this._settings.set_string('color-scheme', 'prefer-light');
+        }
+        // Else: user explicitly chose something; leave it alone
+    }
+
+    _updateSessionColorScheme(scheme) {
+        Main.sessionMode.colorScheme = scheme;
+        St.Settings.get().notify('color-scheme');
     }
 }
